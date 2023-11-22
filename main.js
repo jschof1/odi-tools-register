9437c7b1e5f4fdd556e86678377ddedd7fa6b6ee
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2023-11-22 15:39:53 +0000

    add filterables checkbox

--- main.js
+++ main.js
@@ -1,36 +1,52 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Fetch the JSON data and initialize DataTables
+  var table;
   fetch("./tools.json")
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
-      new DataTable(document.getElementById("toolTable"), {
-        data: data,
-        columns: [
-          { data: "ObjectName" },
-          { data: "Publisher" },
-          { data: "PublisherURL" },
-          { data: "YearPublished" },
-          {
-            data: "ToolURL",
-            render: function (data, type, row) {
-              // Return the data wrapped in an anchor tag
-              return type === "display" && data
-                ? '<span class="tool-url"><a href="' + data + '" target="_blank">Visit webpage</a></span>'
-                : "";
-            },
-          },
-          { data: "Type" },
-          { data: "Language" },
-          { data: "Origin" },
-          { data: "PracticeArea" },
-          { data: "IndustryFocus" },
-          { data: "KeyWords" },
-          { data: "ReportIssue" },
-        ],
+      console.log(data);
+   table = $("#toolTable").DataTable({
+     // Notice we're using the jQuery function here
+     data: data,
+     columns: [
+       { data: "ObjectName" },
+       { data: "Publisher" },
+       { data: "PublisherURL" },
+       { data: "YearPublished" },
+       {
+         data: "ToolURL",
+         render: function (data, type, row) {
+           // Return the data wrapped in an anchor tag
+           return type === "display" && data
+             ? '<span class="tool-url"><a href="' +
+                 data +
+                 '" target="_blank">Visit webpage</a></span>'
+             : "";
+         },
+       },
+       { data: "Type" },
+       { data: "Language" },
+       { data: "Origin" },
+       { data: "PracticeArea" },
+       { data: "IndustryFocus" },
+       { data: "KeyWords" },
+       { data: "ReportIssue" },
+     ],
+   });
+
+      // Ensure checkboxes are set up after the DataTable is initialized
+      var columnCheckboxes = document.querySelectorAll(
+        '#columnToggles input[type="checkbox"]'
+      );
+      columnCheckboxes.forEach(function (checkbox) {
+        checkbox.addEventListener("change", function () {
+          // Get the column index from the data-column attribute
+          var columnIdx = parseInt(this.getAttribute("data-column"), 10);
+          // Toggle the visibility of the column
+          table.column(columnIdx).visible(!table.column(columnIdx).visible());
+        });
       });
-    });
-    
+    })
+    .catch((error) => console.error("Error:", error));
 });
